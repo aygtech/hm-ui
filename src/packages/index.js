650c90e06085f6a98ad4d5376cafa37f3a35569c
remi.zhang <remi.zhang@aiyuangong.com>
2020-02-14 17:23:09 +0800

    select

--- src/packages/index.js
+++ src/packages/index.js
@@ -1,7 +1,9 @@
 import Button from './button'
+import Select from './select'
 
 const components = {
   'hm-button': Button,
+  'hm-select': Select,
 }
 
 export default components
---
+++ src/packages/select/select.vue
@@ -0,0 +1,21 @@
+<template>
+  <a-select
+    showSearch
+    placeholder="请输入搜索关键字"
+    :showArrow="false"
+    :filterOption="false"
+    :defaultActiveFirstOption="false"
+    style="width: 300px;"
+  >
+    <a-select-option value="1">remi</a-select-option>
+    <a-select-option value="2">lzhang</a-select-option>
+  </a-select>
+</template>
+
+<script>
+export default {
+  created() {
+    console.log('Button.props')
+  },
+}
+</script>
